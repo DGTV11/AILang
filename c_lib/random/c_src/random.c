0d43e51506d6052a2bc774e8dc24555b830bf5af
DGTV11 <danielweedk@gmail.com>
2024-01-04 19:13:02 +0800

    Added features to random backend

--- c_lib/random/c_src/random.c
+++ c_lib/random/c_src/random.c
@@ -4,27 +4,38 @@
 
 #include "../include/random.h"
 
-void seed_global_pcgu32(uint64_t seed, uint64_t seq) {
+//i32
+void seed_global_pcgi32(uint64_t seed, uint64_t seq) {
     pcg2_srandom(seed, seq);
 }
 
-void seed_local_pcgu32(pcg32_random_t* rng, uint64_t seed, uint64_t seq) {
+void seed_local_pcgi32(pcg32_random_t* rng, uint64_t seed, uint64_t seq) {
     pcg32_srandom_r(rng, seed, seq);
     return rng;
 }
 
-uint32_t gen_global_pcgu32() {
+int32_t gen_global_pcgi32() {
     return pcg32_random();
 }
 
-uint32_t gen_local_pcgu32(pcg32_random_t* rng) {
+int32_t gen_local_pcgi32(pcg32_random_t* rng) {
     return pcg32_random_r(rng);
 }
 
-uint32_t bounded_gen_global_pcgu32(uint32_t bound) {
+int32_t bounded_gen_global_pcgu32(uint32_t bound) {
     return pcg32_boundedrand(bound);
 }
 
-uint32_t bounded_gen_local_pcgu32(pcg32_random_t* rng, uint32_t bound) {
-    return pcg32_boundedrand_r(rng, bound);
+i32resWboolErr_t bounded_gen_local_pcgi32(pcg32_random_t* rng, int32_t lower_bound, int32_t upper_bound) {
+    i32resWboolErr_t res;
+    res.has_err = false;
+
+    if (lower_bound > upper_bound) {
+        res.has_err = true;
+        return res;
+    } if (lower_bound == upper_bound) {
+        res.n = lower_bound;
+        return res;
+    }
+    return pcg32_boundedrand_r(rng, upper_bound-lower_bound) + lower_bound;
 }
--- c_lib/random/include/random.h
+++ c_lib/random/include/random.h
@@ -2,18 +2,25 @@
 #include "../../external_libraries/float16/c_src/conv.h"
 #include "../../external_libraries/pcg-c-basic-0.9/include/pcg_basic.h"
 #include <stdint.h>
+#include <stdbool.h>
 
 // Typedefs
 typedef float   float32_t;
 typedef double  float64_t;
 
+// Structs
+typedef struct {
+    int32_t n;
+    bool has_err;
+} i32resWboolErr_t;
+
 // Functions
-//*u32
-void seed_global_pcgu32(uint64_t seed, uint64_t seq);
-void seed_local_pcgu32(pcg32_random_t* rng, uint64_t seed, uint64_t seq);
+//*i32
+void seed_global_pcgi32(uint64_t seed, uint64_t seq);
+void seed_local_pcgi32(pcg32_random_t* rng, uint64_t seed, uint64_t seq);
 
-uint32_t gen_global_pcgu32();
-uint32_t gen_local_pcgu32(pcg32_random_t* rng);
+int32_t gen_global_pcgi32();
+int32_t gen_local_pcgi32(pcg32_random_t* rng);
 
-uint32_t bounded_gen_global_pcgu32(uint32_t bound);
-uint32_t bounded_gen_local_pcgu32(pcg32_random_t* rng, uint32_t bound);
+int32_t bounded_gen_global_pcgi32(uint32_t bound);
+int32_t bounded_gen_local_pcgi32(pcg32_random_t* rng, uint32_t bound);
