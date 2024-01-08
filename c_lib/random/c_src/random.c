578465e1bcd6f72f4617baf38c5b7de5e8448c5f
DGTV11 <danielweedk@gmail.com>
2024-01-08 18:56:30 +0800

    Fixed errors in random.c, implemented more C RNGs

--- c_lib/random/c_src/random.c
+++ c_lib/random/c_src/random.c
@@ -4,6 +4,7 @@
 
 #include "../include/random.h"
 
+// Integer RNGs
 //*i32
 void seed_local_pcgi32(pcg32_random_t* rng, uint64_t seed, uint64_t seq) {
     pcg32_srandom_r(rng, seed, seq);
@@ -30,11 +31,11 @@ i32resWboolErr_t bounded_gen_local_pcgi32(pcg32_random_t* rng, int32_t lower_bou
 
 //*i64
 void seed_local_pcgi64(pcg32x2_random_t* rng, uint64_t seed1, uint64_t seed2, uint64_t seq1, uint64_t seq2) {
-    pcg32x2_srandom_r(rng, seed, seq);
+    pcg32x2_srandom_r(rng, seed1, seed2, seq1, seq2);
 }
 
 int64_t gen_local_pcgi64(pcg32x2_random_t* rng) {
-    return pcg32z2_random_r(rng);
+    return pcg32x2_random_r(rng);
 }
 
 i64resWboolErr_t bounded_gen_local_pcgi64(pcg32x2_random_t* rng, int64_t lower_bound, int64_t upper_bound) {
@@ -49,5 +50,72 @@ i64resWboolErr_t bounded_gen_local_pcgi64(pcg32x2_random_t* rng, int64_t lower_b
         return res;
     }
     res.n = (int64_t)pcg32x2_boundedrand_r(rng, upper_bound-lower_bound) + lower_bound; //TODO: TEST!
+    return res;
+}
+
+// Float RNGs (see https://stackoverflow.com/questions/48228749/random-number-generator-pcg-library-how-to-generate-float-numbers-set-within-a, https://stackoverflow.com/questions/52147419/how-to-convert-random-uint64-t-to-random-double-in-range-0-1-using-bit-wise-o)
+//*f16
+float16_t gen_local_pcgf16(pcg32x2_random_t* rng) {
+    uint64_t u64_preprocessed = UINT64_TO_DOUBLE_01_MASK | ((pcg32x2_random_r(rng) >> 12) | 1);
+    return FLOAT64_TO_FLOAT16((REINTERPRET_CAST(float64_t, u64_preprocessed) - 1.0));
+}
+
+f16resWboolErr_t bounded_gen_local_pcgf16(pcg32x2_random_t* rng, float16_t lower_bound, float16_t upper_bound) {
+    f16resWboolErr_t res;
+    res.has_err = false;
+
+    if (f16_gt(lower_bound, upper_bound)) {
+        res.has_err = true;
+        return res;
+    } else if (f16_eq(lower_bound, upper_bound)) {
+        res.n = lower_bound;
+        return res;
+    }
+    float64_t f64_preprocessed = (float64_t)(pcg32x2_random_r(rng))/((float64_t)UINT64_MAX)*FLOAT16_TO_FLOAT64(f16_sub(upper_bound, lower_bound)) + lower_bound;
+    res.n = FLOAT64_TO_FLOAT16(f64_preprocessed);
+    return res;
+}
+
+//*f32
+float32_t gen_local_pcgf32(pcg32x2_random_t* rng) {
+    uint64_t u64_preprocessed = UINT64_TO_DOUBLE_01_MASK | ((pcg32x2_random_r(rng) >> 12) | 1);
+    return (float32_t)(REINTERPRET_CAST(float64_t, u64_preprocessed) - 1.0);
+}
+
+f32resWboolErr_t bounded_gen_local_pcgf32(pcg32x2_random_t* rng, float32_t lower_bound, float32_t upper_bound) {
+    f32resWboolErr_t res;
+    res.has_err = false;
+
+    if (lower_bound > upper_bound) {
+        res.has_err = true;
+        return res;
+    } else if (lower_bound == upper_bound) {
+        res.n = lower_bound;
+        return res;
+    }
+    float64_t f64_preprocessed = (float64_t)(pcg32x2_random_r(rng))/((float64_t)UINT64_MAX)*((float64_t)(upper_bound - lower_bound)) + lower_bound;
+    res.n = (float32_t)f64_preprocessed;
+    return res;
+}
+
+//*f64
+float64_t gen_local_pcgf64(pcg32x2_random_t* rng) {
+    uint64_t u64_preprocessed = UINT64_TO_DOUBLE_01_MASK | ((pcg32x2_random_r(rng) >> 12) | 1);
+    return REINTERPRET_CAST(float64_t, u64_preprocessed) - 1.0;
+}
+
+f64resWboolErr_t bounded_gen_local_pcgf64(pcg32x2_random_t* rng, float64_t lower_bound, float64_t upper_bound) {
+    f64resWboolErr_t res;
+    res.has_err = false;
+
+    if (lower_bound > upper_bound) {
+        res.has_err = true;
+        return res;
+    } else if (lower_bound == upper_bound) {
+        res.n = lower_bound;
+        return res;
+    }
+    float64_t f64_preprocessed = (float64_t)(pcg32x2_random_r(rng))/((float64_t)UINT64_MAX)*(upper_bound - lower_bound) + lower_bound;
+    res.n = f64_preprocessed;
     return res;
 }
--- c_lib/random/include/random.h
+++ c_lib/random/include/random.h
@@ -3,6 +3,17 @@
 #include "../../external_libraries/pcg/include/pcg.h"
 #include <stdint.h>
 #include <stdbool.h>
+#include <limits.h>
+
+// Macros
+#define UINT64_TO_DOUBLE_01_MASK 0x3FEFFFFFFFFFFFFFULL
+#define DOUBLE_UINT64_MAX 0x43F0000000000000
+
+#define REINTERPRET_CAST(tgt_type, value) *(tgt_type*)&value
+#define FLOAT16_TO_FLOAT32(x) float16_to_float32(x)
+#define FLOAT32_TO_FLOAT16(x) float32_to_float16(x)
+#define FLOAT16_TO_FLOAT64(x) (float64_t)float16_to_float32((x))
+#define FLOAT64_TO_FLOAT16(x) float32_to_float16((float32_t)(x))
 
 // Typedefs
 typedef float   float32_t;
@@ -19,10 +30,40 @@ typedef struct {
     bool has_err;
 } i64resWboolErr_t;
 
+typedef struct {
+    float16_t n;
+    bool has_err;
+} f16resWboolErr_t;
+
+typedef struct {
+    float32_t n;
+    bool has_err;
+} f32resWboolErr_t;
+
+typedef struct {
+    float64_t n;
+    bool has_err;
+} f64resWboolErr_t;
+
 // Functions
 //*i32
 void seed_local_pcgi32(pcg32_random_t* rng, uint64_t seed, uint64_t seq);
-
 int32_t gen_local_pcgi32(pcg32_random_t* rng);
-
 i32resWboolErr_t bounded_gen_local_pcgi32(pcg32_random_t* rng, int32_t lower_bound, int32_t upper_bound);
+
+//*i64
+void seed_local_pcgi64(pcg32x2_random_t* rng, uint64_t seed1, uint64_t seed2, uint64_t seq1, uint64_t seq2);
+int64_t gen_local_pcgi64(pcg32x2_random_t* rng);
+i64resWboolErr_t bounded_gen_local_pcgi64(pcg32x2_random_t* rng, int64_t lower_bound, int64_t upper_bound);
+
+//*f16
+float16_t gen_local_pcgf16(pcg32x2_random_t* rng);
+f16resWboolErr_t bounded_gen_local_pcgf16(pcg32x2_random_t* rng, float16_t lower_bound, float16_t upper_bound);
+
+//*f32
+float32_t gen_local_pcgf32(pcg32x2_random_t* rng);
+f32resWboolErr_t bounded_gen_local_pcgf32(pcg32x2_random_t* rng, float32_t lower_bound, float32_t upper_bound);
+
+//*f64
+float64_t gen_local_pcgf64(pcg32x2_random_t* rng);
+f64resWboolErr_t bounded_gen_local_pcgf64(pcg32x2_random_t* rng, float64_t lower_bound, float64_t upper_bound);
