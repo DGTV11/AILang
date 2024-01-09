90bb7cb44353a0ff7ed03d3f9ceccfdc58206cae
DGTV11 <danielweedk@gmail.com>
2024-01-09 18:09:44 +0800

    Implemented float RNGs that use raw pcg32

--- c_lib/random/c_src/random.c
+++ c_lib/random/c_src/random.c
@@ -55,15 +55,14 @@ i64resWboolErr_t bounded_gen_local_pcgi64(pcg32x2_random_t* rng, int64_t lower_b
 
 // Float RNGs (see https://stackoverflow.com/questions/48228749/random-number-generator-pcg-library-how-to-generate-float-numbers-set-within-a, https://stackoverflow.com/questions/52147419/how-to-convert-random-uint64-t-to-random-double-in-range-0-1-using-bit-wise-o)
 //*f16
-float16_t gen_local_pcgf16(pcg32x2_random_t* rng) {
+float16_t gen_local_pcgf16_rng32x2(pcg32x2_random_t* rng) {
     uint64_t u64_preprocessed = UINT64_TO_DOUBLE_01_MASK | ((pcg32x2_random_r(rng) >> 12) | 1);
     return FLOAT64_TO_FLOAT16((REINTERPRET_CAST(float64_t, u64_preprocessed) - 1.0));
 }
 
-f16resWboolErr_t bounded_gen_local_pcgf16(pcg32x2_random_t* rng, float16_t lower_bound, float16_t upper_bound) {
+f16resWboolErr_t bounded_gen_local_pcgf16_rng32x2(pcg32x2_random_t* rng, float16_t lower_bound, float16_t upper_bound) {
     f16resWboolErr_t res;
     res.has_err = false;
-
     if (f16_gt(lower_bound, upper_bound)) {
         res.has_err = true;
         return res;
@@ -71,21 +70,42 @@ f16resWboolErr_t bounded_gen_local_pcgf16(pcg32x2_random_t* rng, float16_t lower
         res.n = lower_bound;
         return res;
     }
-    float64_t f64_preprocessed = (float64_t)(pcg32x2_random_r(rng))/((float64_t)UINT64_MAX)*FLOAT16_TO_FLOAT64(f16_sub(upper_bound, lower_bound)) + lower_bound;
+
+    float64_t f64_preprocessed = (float64_t)(pcg32x2_random_r(rng))/(DOUBLE_UINT64_MAX)*FLOAT16_TO_FLOAT64(f16_sub(upper_bound, lower_bound)) + lower_bound;
     res.n = FLOAT64_TO_FLOAT16(f64_preprocessed);
     return res;
 }
 
+float16_t gen_local_pcgf16_rng32(pcg32_random_t* rng) {
+    uint32_t u32_preprocessed = UINT32_TO_SINGLE_01_MASK | ((pcg32_random_r(rng) >> 9) | 1);
+    return FLOAT32_TO_FLOAT16((REINTERPRET_CAST(float32_t, u32_preprocessed) - 1.0f));
+}
+
+f16resWboolErr_t bounded_gen_local_pcgf16_rng32(pcg32_random_t* rng, float16_t lower_bound, float16_t upper_bound) {
+    f16resWboolErr_t res;
+    res.has_err = false;
+    if (f16_gt(lower_bound, upper_bound)) {
+        res.has_err = true;
+        return res;
+    } else if (f16_eq(lower_bound, upper_bound)) {
+        res.n = lower_bound;
+        return res;
+    }
+
+    float32_t f32_preprocessed = (float32_t)(pcg32_random_r(rng))/(SINGLE_UINT32_MAX)*FLOAT16_TO_FLOAT32(f16_sub(upper_bound, lower_bound)) + lower_bound;
+    res.n = FLOAT32_TO_FLOAT16(f32_preprocessed);
+    return res;
+}
+
 //*f32
-float32_t gen_local_pcgf32(pcg32x2_random_t* rng) {
+float32_t gen_local_pcgf32_rng32x2(pcg32x2_random_t* rng) {
     uint64_t u64_preprocessed = UINT64_TO_DOUBLE_01_MASK | ((pcg32x2_random_r(rng) >> 12) | 1);
     return (float32_t)(REINTERPRET_CAST(float64_t, u64_preprocessed) - 1.0);
 }
 
-f32resWboolErr_t bounded_gen_local_pcgf32(pcg32x2_random_t* rng, float32_t lower_bound, float32_t upper_bound) {
+f32resWboolErr_t bounded_gen_local_pcgf32_rng32x2(pcg32x2_random_t* rng, float32_t lower_bound, float32_t upper_bound) {
     f32resWboolErr_t res;
     res.has_err = false;
-
     if (lower_bound > upper_bound) {
         res.has_err = true;
         return res;
@@ -93,21 +113,20 @@ f32resWboolErr_t bounded_gen_local_pcgf32(pcg32x2_random_t* rng, float32_t lower
         res.n = lower_bound;
         return res;
     }
-    float64_t f64_preprocessed = (float64_t)(pcg32x2_random_r(rng))/((float64_t)UINT64_MAX)*((float64_t)(upper_bound - lower_bound)) + lower_bound;
+
+    float64_t f64_preprocessed = (float64_t)(pcg32x2_random_r(rng))/(DOUBLE_UINT64_MAX)*((float64_t)(upper_bound - lower_bound)) + lower_bound;
     res.n = (float32_t)f64_preprocessed;
     return res;
 }
 
-//*f64
-float64_t gen_local_pcgf64(pcg32x2_random_t* rng) {
-    uint64_t u64_preprocessed = UINT64_TO_DOUBLE_01_MASK | ((pcg32x2_random_r(rng) >> 12) | 1);
-    return REINTERPRET_CAST(float64_t, u64_preprocessed) - 1.0;
+float32_t gen_local_pcgf32_rng32(pcg32_random_t* rng) {
+    uint32_t u32_preprocessed = UINT32_TO_SINGLE_01_MASK | ((pcg32_random_r(rng) >> 9) | 1);
+    return REINTERPRET_CAST(float32_t, u32_preprocessed) - 1.0f;
 }
 
-f64resWboolErr_t bounded_gen_local_pcgf64(pcg32x2_random_t* rng, float64_t lower_bound, float64_t upper_bound) {
-    f64resWboolErr_t res;
+f32resWboolErr_t bounded_gen_local_pcgf32_rng32(pcg32_random_t* rng, float32_t lower_bound, float32_t upper_bound) {
+    f32resWboolErr_t res;
     res.has_err = false;
-
     if (lower_bound > upper_bound) {
         res.has_err = true;
         return res;
@@ -115,7 +134,51 @@ f64resWboolErr_t bounded_gen_local_pcgf64(pcg32x2_random_t* rng, float64_t lower
         res.n = lower_bound;
         return res;
     }
+
+    float32_t f32_preprocessed = (float32_t)(pcg32_random_r(rng))/(SINGLE_UINT32_MAX)*(upper_bound - lower_bound) + lower_bound;
+    res.n = f32_preprocessed;
+    return res;
+}
+
+//*f64
+float64_t gen_local_pcgf64_rng32x2(pcg32x2_random_t* rng) {
+    uint64_t u64_preprocessed = UINT64_TO_DOUBLE_01_MASK | ((pcg32x2_random_r(rng) >> 12) | 1);
+    return REINTERPRET_CAST(float64_t, u64_preprocessed) - 1.0;
+}
+
+f64resWboolErr_t bounded_gen_local_pcgf64_rng32x2(pcg32x2_random_t* rng, float64_t lower_bound, float64_t upper_bound) {
+    f64resWboolErr_t res;
+    res.has_err = false;
+    if (lower_bound > upper_bound) {
+        res.has_err = true;
+        return res;
+    } else if (lower_bound == upper_bound) {
+        res.n = lower_bound;
+        return res;
+    }
+
     float64_t f64_preprocessed = (float64_t)(pcg32x2_random_r(rng))/((float64_t)UINT64_MAX)*(upper_bound - lower_bound) + lower_bound;
     res.n = f64_preprocessed;
+    return res;
+}
+
+float64_t gen_local_pcgf64_rng32(pcg32_random_t* rng) {
+    uint32_t u32_preprocessed = UINT32_TO_SINGLE_01_MASK | ((pcg32_random_r(rng) >> 9) | 1);
+    return (float64_t)(REINTERPRET_CAST(float32_t, u32_preprocessed) - 1.0f);
+}
+
+f64resWboolErr_t bounded_gen_local_pcgf64_rng32(pcg32_random_t* rng, float64_t lower_bound, float64_t upper_bound) {
+    f64resWboolErr_t res;
+    res.has_err = false;
+    if (lower_bound > upper_bound) {
+        res.has_err = true;
+        return res;
+    } else if (lower_bound == upper_bound) {
+        res.n = lower_bound;
+        return res;
+    }
+
+    float32_t f32_preprocessed = (float32_t)(pcg32_random_r(rng))/(SINGLE_UINT32_MAX)*(upper_bound - lower_bound) + lower_bound;
+    res.n = (float64_t)f32_preprocessed;
     return res;
 }
--- c_lib/random/include/random.h
+++ c_lib/random/include/random.h
@@ -7,13 +7,23 @@
 
 // Macros
 #define UINT64_TO_DOUBLE_01_MASK 0x3FEFFFFFFFFFFFFFULL
-#define DOUBLE_UINT64_MAX 0x43F0000000000000
+#define UINT32_TO_SINGLE_01_MASK 0x3F7FFFFFU
 
 #define REINTERPRET_CAST(tgt_type, value) *(tgt_type*)&value
 #define FLOAT16_TO_FLOAT32(x) float16_to_float32(x)
 #define FLOAT32_TO_FLOAT16(x) float32_to_float16(x)
 #define FLOAT16_TO_FLOAT64(x) (float64_t)float16_to_float32((x))
 #define FLOAT64_TO_FLOAT16(x) float32_to_float16((float32_t)(x))
+
+static const uint32_t SINGLE_UINT32_MAX_BITS = 0x4F800000U;
+static const uint32_t SINGLE_UINT64_MAX_BITS = 0x5F800000U;
+static const uint64_t DOUBLE_UINT32_MAX_BITS = 0x41EFFFFFFFE00000ULL;
+static const uint64_t DOUBLE_UINT64_MAX_BITS = 0x43F0000000000000ULL;
+
+#define SINGLE_UINT32_MAX REINTERPRET_CAST(uint32_t, SINGLE_UINT32_MAX_BITS)
+#define SINGLE_UINT64_MAX REINTERPRET_CAST(uint32_t, SINGLE_UINT64_MAX_BITS)
+#define DOUBLE_UINT32_MAX REINTERPRET_CAST(uint64_t, DOUBLE_UINT32_MAX_BITS)
+#define DOUBLE_UINT64_MAX REINTERPRET_CAST(uint64_t, DOUBLE_UINT64_MAX_BITS)
 
 // Typedefs
 typedef float   float32_t;
@@ -57,12 +67,18 @@ int64_t gen_local_pcgi64(pcg32x2_random_t* rng);
 i64resWboolErr_t bounded_gen_local_pcgi64(pcg32x2_random_t* rng, int64_t lower_bound, int64_t upper_bound);
 
 //*f16
-float16_t gen_local_pcgf16(pcg32x2_random_t* rng);
-f16resWboolErr_t bounded_gen_local_pcgf16(pcg32x2_random_t* rng, float16_t lower_bound, float16_t upper_bound);
+float16_t gen_local_pcgf16_rng32x2(pcg32x2_random_t* rng);
+f16resWboolErr_t bounded_gen_local_pcgf16_rng32x2(pcg32x2_random_t* rng, float16_t lower_bound, float16_t upper_bound);
+
+float16_t gen_local_pcgf16_rng32(pcg32_random_t* rng);
+f16resWboolErr_t bounded_gen_local_pcgf16_rng32(pcg32_random_t* rng, float16_t lower_bound, float16_t upper_bound);
 
 //*f32
-float32_t gen_local_pcgf32(pcg32x2_random_t* rng);
-f32resWboolErr_t bounded_gen_local_pcgf32(pcg32x2_random_t* rng, float32_t lower_bound, float32_t upper_bound);
+float32_t gen_local_pcgf32_rng32x2(pcg32x2_random_t* rng);
+f32resWboolErr_t bounded_gen_local_pcgf32_rng32x2(pcg32x2_random_t* rng, float32_t lower_bound, float32_t upper_bound);
+
+float32_t gen_local_pcgf32_rng32(pcg32_random_t* rng);
+f32resWboolErr_t bounded_gen_local_pcgf32_rng32(pcg32_random_t* rng, float32_t lower_bound, float32_t upper_bound);
 
 //*f64
 float64_t gen_local_pcgf64(pcg32x2_random_t* rng);
